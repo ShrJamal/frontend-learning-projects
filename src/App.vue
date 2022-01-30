32ea550c2ca17231307733916f3b95ca877311d2
Jamal .S <me@shrjamal.com>
2022-01-30 23:20:48 +0100

    add global styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,41 @@
 <template>
   <router-view />
 </template>
+
+<style>
+:root {
+  --color-background: #181818;
+  --color-text: white;
+}
+
+@media (prefers-color-scheme: ligh) {
+  :root {
+    --color-background: white;
+    --color-text: #181818;
+  }
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  position: relative;
+  font-weight: normal;
+}
+
+body {
+  min-height: 100vh;
+  color: var(--color-text);
+  background: var(--color-background);
+  transition: color 0.5s, background-color 0.5s;
+  line-height: 1.6;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  font-size: 15px;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
